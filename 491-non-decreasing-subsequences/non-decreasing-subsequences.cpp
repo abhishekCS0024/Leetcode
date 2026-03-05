acd4fdac6afac2d26b69ef4408d2f0ee930b001d
Abhishek Kumar <157698385+abhishekCS0024@users.noreply.github.com>
2026-03-05 23:12:44 +0530

    Time: 35 ms (44.73%) | Memory: 29.9 MB (38.05%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.cpp
@@ -1,29 +1,65 @@
 class Solution {
 public:
-    set<vector<int>> result;
+    vector<vector<int>> result;
     int n;
 
-    void fun(vector<int>& nums, int i, vector<int> curr){
+    void fun(vector<int>& nums, int start, vector<int> &curr){
 
-        if(curr.size()>=2 && i==n){
-            result.insert(curr);
-            return;
+        if(curr.size()>=2){
+            result.push_back(curr);
+            // return;
         }
 
-        if(i>=n) return;
+        unordered_set<int>st;
 
-        if(curr.empty() || curr.back()<=nums[i]){
+        for(int i=start;i<n;i++){
+            if(st.count(nums[i])) continue;
+            if(curr.empty() || curr.back()<=nums[i]){
+            st.insert(nums[i]);
             curr.push_back(nums[i]);
             fun(nums,i+1,curr);
             curr.pop_back();
+            
         }
-
-        fun(nums,i+1,curr);
+        }
     }
 
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         n=nums.size();
-        fun(nums,0,{});
-        return vector<vector<int>>(result.begin(),result.end());
+        vector<int>curr;
+        fun(nums,0,curr);
+        return result;
     }
 };
+
+
+// // Pure Recursion
+// class Solution {
+// public:
+//     set<vector<int>> result;
+//     int n;
+
+//     void fun(vector<int>& nums, int i, vector<int> curr){
+
+//         if(curr.size()>=2 && i==n){
+//             result.insert(curr);
+//             return;
+//         }
+
+//         if(i>=n) return;
+
+//         if(curr.empty() || curr.back()<=nums[i]){
+//             curr.push_back(nums[i]);
+//             fun(nums,i+1,curr);
+//             curr.pop_back();
+//         }
+
+//         fun(nums,i+1,curr);
+//     }
+
+//     vector<vector<int>> findSubsequences(vector<int>& nums) {
+//         n=nums.size();
+//         fun(nums,0,{});
+//         return vector<vector<int>>(result.begin(),result.end());
+//     }
+// };
