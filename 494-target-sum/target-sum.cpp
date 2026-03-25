3773327afa4ce3fd43a5eba88b48efdb501741bf
Abhishek Kumar <157698385+abhishekCS0024@users.noreply.github.com>
2026-03-25 19:02:35 +0530

    Time: 41 ms (40.15%) | Memory: 35.4 MB (18.80%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int cnt=0;
-    void fun(vector<int>& nums, int target,int curr,int i){
+
+    int fun(vector<int>& nums, int target,int total,vector<vector<int>>&dp,int i,int sum){
         if(i==nums.size()){
-            if(curr==target) cnt++;
-            return;
+            if(target==sum){
+                return 1;
+            }
+            return 0;
         }
 
-        fun(nums,target,curr+nums[i],i+1);
-        fun(nums,target,curr-nums[i],i+1);
+        if(dp[sum+total][i]!=-1) return dp[sum+total][i];
+        int plus=fun(nums,target,total,dp,i+1,sum+nums[i]);
+        int minus=fun(nums,target,total,dp,i+1,sum-nums[i]);
+
+        return dp[sum+total][i]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        fun(nums,target,0,0);
+        int n=nums.size();
+        int total=0;
 
-        return cnt;
+        for(int i=0;i<n;i++){
+            total+=nums[i];
+        }
+
+        vector<vector<int>>dp((total*2)+1,vector<int>(n+1,-1));
+
+        return fun(nums,target,total,dp,0,0);
     }
 };
